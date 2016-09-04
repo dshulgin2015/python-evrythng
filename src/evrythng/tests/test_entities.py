3e84693bc06bdc57395f641019f99adc27d58eb2
dshulgin2015 <unetby2016@yandex.ru>
2016-09-04 19:19:21 +0300

    actions unit tests have been added

--- src/evrythng/tests/test_entities.py
+++ src/evrythng/tests/test_entities.py
@@ -17,11 +17,8 @@ with open('config.json') as configs:
 
 API_KEY = data['parameters']['API_KEY']
 
-product_id = ''
-
-
-
 class TestProductMethods(unittest.TestCase):
+
 
 	'''testing the following methods:
 
@@ -35,30 +32,81 @@ class TestProductMethods(unittest.TestCase):
 
 	'''
   	def test1_creation(self):
-		global product_id
-		product_id = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
+		product_id  = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
 		product_name = products.read_product(product_id=str(product_id), api_key=API_KEY).json()['name']
 		self.assertEqual(product_name, 'test_creating_prod_GooeeIOT')
+		products.delete_product(product_id=str(product_id), api_key=API_KEY)
 
 	def test2_list(self):
+		product_id  = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
 		self.assertEqual(len(products.list_products(api_key=API_KEY).json()), 1)
+		products.delete_product(product_id=str(product_id), api_key=API_KEY)
 
 	def test3_update(self):
-		global product_id
+		product_id  = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
 		products.update_product(product_id=str(product_id), api_key=API_KEY, description='test_GooeeIOT_description')
 		description = products.read_product(product_id=str(product_id), api_key=API_KEY).json()['description']
 		self.assertEqual(description, 'test_GooeeIOT_description')
+		products.delete_product(product_id=str(product_id), api_key=API_KEY)
 
 	def test4_delete(self):
-		global product_id
+		count = len(products.list_products(api_key=API_KEY).json())
+		product_id  = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
+		count += 1
+		status_code = products.delete_product(product_id=str(product_id), api_key=API_KEY).status_code
+		self.assertEqual(status_code, 200)
+		self.assertEqual(len(products.list_products(api_key=API_KEY).json()), count - 1)
+
+
+
+
+class TestActionMethods(unittest.TestCase):
+
+	'''testing the following methods:
+
+  	create_action+
+  	delete_action+
+  	list_actions
+
+	on a test EVT account by Dmitry Shulgin.
+
+	'''
+	def test1_(self):
+
+		#creating necessary objects required for making test action: thng || product || shortID
+		product_id = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
+		#getting action id from response object
+		action_id = actions.create_action(type_='scans', #need to create more on every base action types.
+										api_key=API_KEY,
+										customFields={'test_custom_Field': 'test_value'}, 
+										product = str(product_id)).json()['id']		
+		#checking if status code == 200 
+		status_code = actions.delete_action(type_='scans', action_id=str(action_id), api_key=API_KEY).status_code #why we need type when we know action id?
+		self.assertEqual(status_code, 200)
 		products.delete_product(product_id=str(product_id), api_key=API_KEY)
-		self.assertEqual(len(products.list_products(api_key=API_KEY).json()), 0)
+
+	def test2_list_actions(self):
+		count = len(actions.list_actions(type_='scans',api_key=API_KEY).json()) #save number of actions before we create new one.
+
+		#creating necessary objects required for making test action: thng || product || shortID 
+		product_id = products.create_product(name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id'] 
+		action_id = actions.create_action(type_='scans',
+										api_key=API_KEY,
+										customFields={'test_custom_Field': 'test_value'}, 
+										product = str(product_id)).json()['id']
+		self.assertEqual(len(actions.list_actions(type_='scans', api_key=API_KEY).json()), count + 1)
+
+		#delete test objects
+		actions.delete_action(type_='scans', action_id=str(action_id), api_key=API_KEY)
+		products.delete_product(product_id=str(product_id), api_key=API_KEY)
+	
 
 
 
-	#P.S I guess, that for all interactions with EVT platform we should come up with smth smarter than test acc
-	#    according testing.
-	#    
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
