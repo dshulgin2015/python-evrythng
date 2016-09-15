b49f046311208c2f0a8f89c52238a9cc229f64a5
dshulgin2015 <unetby2016@yandex.ru>
2016-09-15 21:49:39 +0300

    add API_KEY as env var

--- src/evrythng/tests/test_entities.py
+++ src/evrythng/tests/test_entities.py
@@ -1,7 +1,9 @@
 import unittest
-
+import os
 from evrythng.entities import actions
 from evrythng.entities import products
+
+API_KEY = os.environ['API_KEY']
 
 
 class TestProductMethods(unittest.TestCase):
@@ -11,21 +13,21 @@ class TestProductMethods(unittest.TestCase):
         """Product Tests Setup"""
 
         cls.product_id = products.create_product(
-            name='test_creating_prod_GooeeIOT').json()['id']
+            name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
 
     @classmethod
     def tearDownClass(cls):
         """Product Tests Destroying"""
 
         products.delete_product(
-            product_id=str(cls.product_id))
+            product_id=str(cls.product_id), api_key=API_KEY)
 
     def test1_creation(self):
         """creating test product and
          check if existing name is equal to test str"""
 
         product_name = products.read_product(product_id=str(
-            self.product_id)).json()['name']
+            self.product_id), api_key=API_KEY).json()['name']
         self.assertEqual(product_name, 'test_creating_prod_GooeeIOT')
 
     def test2_list(self):
@@ -33,16 +35,16 @@ class TestProductMethods(unittest.TestCase):
          check if list's length is equal to 1"""
 
         self.assertEqual(
-            len(products.list_products().json()), 1)
+            len(products.list_products(api_key=API_KEY).json()), 1)
 
     def test3_update(self):
         """creating test product, update it and check,
         if product description is equal to what we expect"""
 
         products.update_product(product_id=str(
-            self.product_id), description='test_GooeeIOT_description')
+            self.product_id), description='test_GooeeIOT_description', api_key=API_KEY)
         description = products.read_product(product_id=str(
-            self.product_id)).json()['description']
+            self.product_id), api_key=API_KEY).json()['description']
         self.assertEqual(description, 'test_GooeeIOT_description')
 
 
@@ -53,27 +55,27 @@ class TestActionMethods(unittest.TestCase):
         """Action Tests Setup"""
 
         cls.product_id = products.create_product(
-            name='test_creating_prod_GooeeIOT').json()['id']
+            name='test_creating_prod_GooeeIOT', api_key=API_KEY).json()['id']
         cls.action_id = actions.create_action(type_='scans',
-                                          customFields={
-                                              'test_custom_Field': 'test_value'},
-                                          product=str(cls.product_id)).json()['id']
+                                              customFields={
+                                                  'test_custom_Field': 'test_value'},
+                                        product=str(cls.product_id), api_key=API_KEY).json()['id']
 
     @classmethod
     def tearDownClass(cls):
         """Action Tests Destroying"""
 
         products.delete_product(
-            product_id=str(cls.product_id))
+            product_id=str(cls.product_id), api_key=API_KEY)
         actions.delete_action(
-            type_='scans', action_id=str(cls.action_id))
+            type_='scans', action_id=str(cls.action_id), api_key=API_KEY)
 
     def test_list_actions(self):
-        """creating test product and 
+        """creating test product and
         check if list's length is decreased"""
 
         self.assertEqual(len(actions.list_actions(
-            type_='scans').json()), 1)
+            type_='scans', api_key=API_KEY).json()), 1)
 
 
 if __name__ == '__main__':
